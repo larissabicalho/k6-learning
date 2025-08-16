3f2529bd19167cc5ff05767d496eb2a47dcdd7dd
larissaBicalho <trc6017760@unimedbh.com.br>
2025-08-16 16:50:38 -0300

    primeiro teste

---
+++ script/test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import {sleep, check} from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 10 }, 
+        { duration: '10s', target: 10 },
+        { duration: '10s', target: 0 } 
+    ],
+    thresholds: {
+        checks: ['rate > 0.95'],
+        http_req_duration: ['p(95) < 400']
+    },
+    ext:{
+        loadimpact:{
+            projectID: '3884820',
+            name: 'POC CURSO K6'
+        }
+    }
+}
+
+export default function(){
+
+    const BASE_URL = 'https://www.vagalume.com.br/u2/index.js';
+    const res = http.get(BASE_URL)
+    
+    check(res,{
+        'status 200': (r) => r.status === 200
+    });
+    sleep(1);
+}
